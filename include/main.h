6c27e170440fd3ff5e052b4159ebd43299837784
ThingEngineer <josh.lee.campbell@gmail.com>
2018-10-18 15:22:54 -0500

    Add header file and include

---
+++ include/main.h
@@ -0,0 +1,42 @@
+/*
+ * main.h
+ *
+ * Author: Josh Campbell
+ */
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) < 303
+#error "AVR-GCC 3.3 or later is required, update AVR-GCC compiler!"
+#endif
+
+
+/****************************************************************************
+* Includes
+****************************************************************************/
+#include "env.h"                       // Sensitive environment parameters
+#include <Arduino.h>                   // Main include file for the Arduino SDK
+#include <WiFi.h>                      // ESP32 Wifi support
+#include <HTTPClient.h>                // Library to easily make HTTP GET, POST and PUT requests to a web server
+#include <WiFiClientSecure.h>          // Base class that provides Client SSL to ESP32
+
+
+/****************************************************************************
+* Function prototypes
+****************************************************************************/
+void doHttpPost(void);
+
+
+/****************************************************************************
+* Port aliases
+****************************************************************************/
+
+
+/****************************************************************************
+* Variable and constant declarations
+****************************************************************************/
+
+
+#endif /* MAIN_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <Arduino.h>
+#include "main.h"
+
 
 void setup() {
     // put your setup code here, to run once:
